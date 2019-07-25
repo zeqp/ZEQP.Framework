08608371a347a1e0a150e6749f0bacc614d0dae7
刘巨 <liuju150@outlook.com>
2019-07-25 10:32:06 +0800

    BaseRepository

--- ZEQP.Framework/Repository/BaseRepository.cs
+++ ZEQP.Framework/Repository/BaseRepository.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZEQP.Framework
 {
-    public class BaseRepository
+    public class BaseRepository : IBaseRepository
     {
+        public DbContext Context { get; }
+        public BaseRepository(DbContext context)
+        {
+            this.Context = context;
+        }
+        public DbSet<T> Set<T>() where T : class => this.Context.Set<T>();
+        public IDbContextTransaction Transaction => this.Context.Database.CurrentTransaction;
+        public void BeginTransaction() => this.Context.Database.BeginTransaction();
+        public Task<IDbContextTransaction> BeginTransactionAsync() => this.Context.Database.BeginTransactionAsync();
+        public void Commit(bool rollback = false)
+        {
+            if (!rollback)
+            {
+                this.SaveChanges();
+                this.Transaction?.Commit();
+                return;
+            }
+            this.Transaction?.Rollback();
+        }
+        public int SaveChanges() => this.Context.SaveChanges();
+        public Task<int> SaveChangesAsync() => this.Context.SaveChangesAsync();
     }
 }
--- ZEQP.Framework/Repository/IBaseRepository.cs
+++ ZEQP.Framework/Repository/IBaseRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZEQP.Framework
 {
     public interface IBaseRepository
     {
+        #region Transaction
+        IDbContextTransaction Transaction { get; }
+        void BeginTransaction();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        void Commit(bool rollback = false);
+        #endregion
+        DbContext Context { get; }
+        DbSet<T> Set<T>() where T : class;
+
+        #region SaveChanges
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        #endregion
     }
 }
