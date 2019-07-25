f5d23525017e5836a61b363b9c88c73a357aa5a3
刘巨 <liuju150@outlook.com>
2019-07-25 14:49:16 +0800

    BaseRepository

--- ZEQP.Framework/Repository/BaseRepository.cs
+++ ZEQP.Framework/Repository/BaseRepository.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,8 @@ namespace ZEQP.Framework
             this.Context = context;
         }
         public DbSet<T> Set<T>() where T : class => this.Context.Set<T>();
+
+        #region Transaction
         public IDbContextTransaction Transaction => this.Context.Database.CurrentTransaction;
         public void BeginTransaction() => this.Context.Database.BeginTransaction();
         public Task<IDbContextTransaction> BeginTransactionAsync() => this.Context.Database.BeginTransactionAsync();
@@ -28,7 +32,78 @@ namespace ZEQP.Framework
             }
             this.Transaction?.Rollback();
         }
+        #endregion
+
+        #region SaveChanges
         public int SaveChanges() => this.Context.SaveChanges();
         public Task<int> SaveChangesAsync() => this.Context.SaveChangesAsync();
+        #endregion
+
+        #region Get
+        public T Get<T>(object id, bool track = true)
+            where T : class
+        {
+            if (!track)
+                this.Context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            var entity = this.Set<T>().Find(id);
+            this.Context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+            return entity;
+        }
+        public async Task<T> GetAsync<T>(object id, bool track = true)
+            where T : class
+        {
+            if (!track)
+                this.Context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            var entity = await this.Set<T>().FindAsync(id);
+            this.Context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+            return entity;
+        }
+        public T Get<T>(Expression<Func<T, bool>> predicate, bool track = true)
+            where T : class
+        {
+            if (track)
+                return this.Set<T>().Where(predicate).SingleOrDefault();
+            return this.Set<T>().AsNoTracking().Where(predicate).SingleOrDefault();
+        }
+        public Task<T> GetAsync<T>(Expression<Func<T, bool>> predicate, bool track = true)
+            where T : class
+        {
+            if (track)
+                return this.Set<T>().Where(predicate).SingleOrDefaultAsync();
+            return this.Set<T>().AsNoTracking().Where(predicate).SingleOrDefaultAsync();
+        }
+        #endregion
+
+        #region GetList
+
+        public List<T> GetList<T>(Expression<Func<T, bool>> predicate, bool track = true)
+            where T : class
+        {
+            if (track)
+                return this.Set<T>().Where(predicate).ToList();
+            return this.Set<T>().AsNoTracking().Where(predicate).ToList();
+        }
+        public Task<List<T>> GetListAsync<T>(Expression<Func<T, bool>> predicate, bool track = true)
+            where T : class
+        {
+            if (track)
+                return this.Set<T>().Where(predicate).ToListAsync();
+            return this.Set<T>().AsNoTracking().Where(predicate).ToListAsync();
+        }
+        public List<T> GetList<T>(IQueryable<T> queryable)
+            where T : class
+        {
+            return queryable.ToList();
+        }
+        public Task<List<T>> GetListAsync<T>(IQueryable<T> queryable)
+            where T : class
+        {
+            return queryable.ToListAsync();
+        }
+        #endregion
+
+        #region GetPage
+
+        #endregion
     }
 }
--- ZEQP.Framework/Repository/IBaseRepository.cs
+++ ZEQP.Framework/Repository/IBaseRepository.cs
@@ -2,25 +2,102 @@
 using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZEQP.Framework
 {
+    /// <summary>
+    /// 基本仓储接口
+    /// </summary>
     public interface IBaseRepository
     {
         #region Transaction
+        /// <summary>
+        /// 事务
+        /// </summary>
         IDbContextTransaction Transaction { get; }
+        /// <summary>
+        /// 开始一个新事务
+        /// </summary>
         void BeginTransaction();
+        /// <summary>
+        /// 开始一个新事务
+        /// </summary>
+        /// <returns></returns>
         Task<IDbContextTransaction> BeginTransactionAsync();
+        /// <summary>
+        /// 提交事务
+        /// </summary>
+        /// <param name="rollback"></param>
         void Commit(bool rollback = false);
         #endregion
+        /// <summary>
+        /// 当前数据库上下文
+        /// </summary>
         DbContext Context { get; }
+        /// <summary>
+        /// 当前数据表
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         DbSet<T> Set<T>() where T : class;
 
         #region SaveChanges
+        /// <summary>
+        /// 保存数据变更
+        /// </summary>
+        /// <returns></returns>
         int SaveChanges();
+        /// <summary>
+        /// 保存数据变更
+        /// </summary>
+        /// <returns></returns>
         Task<int> SaveChangesAsync();
+        #endregion
+
+        #region Get
+        /// <summary>
+        /// 根据ID，拿到一个实体
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="id">主键ID</param>
+        /// <param name="track">是否跟踪</param>
+        /// <returns>数据库实体</returns>
+        T Get<T>(object id, bool track = true) where T : class;
+        /// <summary>
+        /// 根据ID，拿到一个实体
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="id">主键ID</param>
+        /// <param name="track">是否跟踪</param>
+        /// <returns>数据库实体</returns>
+        Task<T> GetAsync<T>(object id, bool track = true) where T : class;
+        /// <summary>
+        /// 根据查询条件,拿到数据库中唯一的实体
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="predicate">查询条件</param>
+        /// <param name="track">是否跟踪</param>
+        /// <returns>数据库实体</returns>
+        T Get<T>(Expression<Func<T, bool>> predicate, bool track = true) where T : class;
+        /// <summary>
+        /// 根据查询条件,拿到数据库中唯一的实体
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="predicate">查询条件</param>
+        /// <param name="track">是否跟踪</param>
+        /// <returns>数据库实体</returns>
+        Task<T> GetAsync<T>(Expression<Func<T, bool>> predicate, bool track = true) where T : class;
+        #endregion
+
+        #region GetList
+        List<T> GetList<T>(Expression<Func<T, bool>> predicate, bool track = true) where T : class;
+        Task<List<T>> GetListAsync<T>(Expression<Func<T, bool>> predicate, bool track = true) where T : class;
+        List<T> GetList<T>(IQueryable<T> queryable) where T : class;
+        Task<List<T>> GetListAsync<T>(IQueryable<T> queryable) where T : class;
         #endregion
     }
 }
