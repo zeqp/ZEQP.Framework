58e51bda37ddc9293b829e7f6eb84bad847be47b
刘巨 <liuju150@outlook.com>
2019-08-05 15:25:38 +0800

    Delete

--- ZEQP.Framework/Repository/BusRepository_Key.cs
+++ ZEQP.Framework/Repository/BusRepository_Key.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZEQP.Framework
 {
@@ -12,5 +14,36 @@ namespace ZEQP.Framework
         public BusRepository(DbContext context, IMapper mapper)
             : base(context, mapper)
         { }
+
+        public override bool Delete(object id)
+        {
+            var entity = this.Get(id);
+            entity.Deleted = true;
+            return this.Update(entity, true, new List<string>() { nameof(entity.Deleted) });
+
+        }
+        public override async Task<bool> DeleteAsync(object id)
+        {
+            var entity = await this.GetAsync(id);
+            entity.Deleted = true;
+            return await this.UpdateAsync(entity, true, new List<string>() { nameof(entity.Deleted) });
+        }
+        public override bool Delete(List<K> ids)
+        {
+            var list = this.GetList(ids);
+            list.ForEach((entity) =>
+            {
+                entity.Deleted = true;
+            });
+            return this.Update(list, true, new List<string>() { "Deleted" });
+        }
+        public override async Task<bool> DeleteAsync(List<K> ids)
+        {
+            var list = await this.GetListAsync(ids);
+            list.ForEach((entity) => {
+                entity.Deleted = true;
+            });
+            return await this.UpdateAsync(list, true, new List<string>() { "Deleted" });
+        }
     }
 }
