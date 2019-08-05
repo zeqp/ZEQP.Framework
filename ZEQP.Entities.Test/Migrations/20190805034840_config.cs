c23cc816b2f4a6779db6322aa36131d96b7112ee
刘巨 <liuju150@outlook.com>
2019-08-05 11:49:33 +0800

    db

---
+++ ZEQP.Entities.Test/Migrations/20190805034840_config.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZEQP.Entities.Test.Migrations
+{
+    public partial class config : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
