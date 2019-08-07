657082487d8b5e3f559b05f671f2e899cbcf2160
刘巨 <liuju150@outlook.com>
2019-08-07 10:06:55 +0800

    ObjectId

---
+++ ZEQP.Framework.Test/ObjectIdTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ZEQP.Framework;
+
+namespace ZEQP.Framework.Test
+{
+    [TestClass]
+    public class ObjectIdTest1
+    {
+        [TestMethod]
+        public void CreateID()
+        {
+            Thread.ParallelExecute(() =>
+            {
+                Console.WriteLine($"{Task.CurrentId}(ObjectId)\t{ObjectId.GenerateNewId()}");
+                Console.WriteLine($"{Task.CurrentId}(String)\t{ObjectId.GenerateNewStringId()}");
+            }, 100);
+        }
+    }
+}
